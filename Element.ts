158c6fe0871b9056d44972f5c0f35a4f836b9ee1
dimp10 <65302354+dimp10@users.noreply.github.com>
2023-05-19 19:32:24 +0100

    Update pxt.json, main.ts, Element.ts

---
+++ Element.ts
@@ -0,0 +1,31 @@
+class Element {
+    name: String;
+    combos: { [key: string]: string };
+    sprite: Image;
+    scale: number;
+
+    constructor(name: String, combos: { [key: string]: string} = {}, sprite: Image, scale: number) {
+        this.name = name;
+        this.combos = combos;
+        this.sprite = sprite;
+        this.scale = scale;
+    }
+
+    toSprite(): Sprite {
+        let sprite: Sprite = sprites.create(this.sprite, SpriteKind.Element)
+        sprite.data = this;
+        sprite.setScale(1 / this.scale, ScaleAnchor.Middle);
+        return sprite
+    }
+
+    merge(self: Sprite, other: Sprite): Sprite | false {
+        if (self.data.combos[other.data.name] !== undefined) {
+            let out: Sprite = elementList[self.data.combos[other.data.name]].toSprite()
+            out.x = self.x;
+            out.y = self.y;
+            out.data.scale = elementList[self.data.combos[other.data.name]].scale
+            return out
+        }
+        return false
+    }
+}
--- main.ts
+++ main.ts
@@ -1,29 +1,3 @@
 namespace SpriteKind {
-    export const element = SpriteKind.create()
+    export const Element = SpriteKind.create()
 }
-
-let banned_from_artwork = ["georgi"]
-// sprites
-let air = sprites.create(assets.image`Air`, SpriteKind.element)
-air.setPosition(20, 20)
-SpriteKind.element
-let water = sprites.create(assets.image`water`, SpriteKind.element)
-water.setPosition(120, 100)
-water.data = [1, 2, 3, 4, 5]
-let fire = sprites.create(assets.image`fire`, SpriteKind.element)
-fire.setPosition(20, 100)
-let earth = sprites.create(assets.image`Earth`, SpriteKind.element)
-earth.setPosition(120, 20)
-let cursor = sprites.create(assets.image`Cursor`, SpriteKind.Player)
-cursor.setScale(1, ScaleAnchor.Middle)
-controller.moveSprite(cursor)
-sprites.onOverlap(SpriteKind.element, SpriteKind.Player, function pick_up(element: Sprite, cursor: Sprite) {
-    if (controller.A.isPressed()) {
-        element.setPosition(cursor.x, cursor.y)
-    }
-    
-})
-scene.onOverlapTile(SpriteKind.element, fire, function on_overlap_tile(water: Sprite, fire: tiles.Location) {
-    let steam = sprites.create(assets.image`Steam`, SpriteKind.element)
-})
-database.listAddValue("", 0)
